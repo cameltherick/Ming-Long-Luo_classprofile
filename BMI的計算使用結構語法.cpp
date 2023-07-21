b79868faa964cf156621935d283f6c7e0d7c76f8
cameltherick <105897248+cameltherick@users.noreply.github.com>
2023-07-21 22:43:11 +0800

    Update BMI的計算使用結構語法.cpp

--- BMI的計算使用結構語法.cpp
+++ BMI的計算使用結構語法.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+struct infor{
+        double w;
+        double h;
+};
+
+double BMI(double h, double w)
+{
+    return w/(h*h);
+}
+int main()
+{
+    infor person;
+    cin>>person.w;
+    cin>>person.h;
+    person.h/=100;
+    double bmi = BMI(person.h, person.w);
+    bmi=round(bmi*10)/10;
+    cout<<bmi<<"\n";
+    if(bmi<18.5) cout<<"Lightweight";
+    else if(bmi<24) cout<<"perfect";
+    else if(bmi<27) cout<<"Moderately obese";
+    else cout<<"fatty";
+    return 0;
+}
